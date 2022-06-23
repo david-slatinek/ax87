78ef2d1b89fb175bf836bba96bf25506994cf81c
David Slatinek <79467409+david-slatinek@users.noreply.github.com>
2022-06-23 14:38:43 +0200

    Remove version flag, add validation for read flag argument

--- arduino/main.go
+++ arduino/main.go
@@ -3,21 +3,20 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"os"
 )
 
-const version float32 = 1.0
-
 func main() {
-	readPtr := flag.Int("read", 0, "Read sensor value.\n\t0 = all\n\t1 = carbon monoxide\n\t2 = air quality\n\t3 = raindrops\n\t4 = soil moisture")
-	printPtr := flag.Bool("print", true, "Print values")
-	versionPtr := flag.Bool("version", false, "Print version")
+	readPtr := flag.Int("read", 0, "Read sensor value/values\n\t0 = all (default)\n\t1 = carbon monoxide\n\t2 = air quality\n\t3 = raindrops\n\t4 = soil moisture")
+	printPtr := flag.Bool("print", true, "Print read value/values")
 
 	flag.Parse()
 
-	if *versionPtr {
-		fmt.Println("Version:", version)
-		os.Exit(0)
+	if *readPtr < 0 || *readPtr > 4 {
+		log.Println("Invalid value for -read argument!")
+		log.Println("Exiting...")
+		os.Exit(1)
 	}
 
 	fmt.Println(*readPtr)
