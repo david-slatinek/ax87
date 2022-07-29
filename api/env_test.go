9c0b22e96b587a4a4c976701d392d7ed5b5d2769
David Slatinek <79467409+david-slatinek@users.noreply.github.com>
2022-07-29 18:26:27 +0200

    Update env_test.go

--- api/env_test.go
+++ api/env_test.go
@@ -6,16 +6,14 @@ import (
 
 // Test Load.
 func TestLoad(t *testing.T) {
-	err := Load("test.env")
-	if err != nil {
+	if err := Load("test.env"); err != nil {
 		t.Errorf("Expected nil with Load, got %v", err)
 	}
 }
 
 // Test Validate.
 func TestValidate(t *testing.T) {
-	err := Validate()
-	if err != nil {
+	if err := Validate(); err != nil {
 		t.Errorf("Expected nil with Validate, got %v", err)
 	}
 }
