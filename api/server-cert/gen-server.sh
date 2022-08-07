5d5ac28657548eb12551bceb9c40ae1f25826999
David Slatinek <79467409+david-slatinek@users.noreply.github.com>
2022-08-07 16:49:37 +0200

    Move files

--- api/server-cert/gen-server.sh
+++ api/server-cert/gen-server.sh
@@ -16,10 +16,10 @@ openssl req -new -key server-key.pem -out server-req.pem -config server-config.c
 # openssl req -text -in server-req.pem -noout
 
 # Create a self-signed certificate
-openssl x509 -req -in server-req.pem -extfile server-ext.cnf -days 60 -CA ../ca/ca-cert.pem -CAkey ../ca/ca-key.pem -CAcreateserial -out server-cert.pem
+openssl x509 -req -in server-req.pem -extfile server-ext.cnf -days 60 -CA ../../ca-cert/ca-cert.pem -CAkey ../../ca-cert/ca-key.pem -CAcreateserial -out server-cert.pem
 
 # View certificate
 # openssl x509 -text -in server-cert.pem -noout
 
 # Verify certificate
-openssl verify -CAfile ../ca/ca-cert.pem server-cert.pem
+openssl verify -CAfile ../../ca-cert/ca-cert.pem server-cert.pem
