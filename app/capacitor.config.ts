c8ae2a1c1044dbfbdd92b60b596b1f531d08dfe5
David Slatinek <79467409+david-slatinek@users.noreply.github.com>
2022-08-28 11:41:38 +0200

    Update packages

--- app/capacitor.config.ts
+++ app/capacitor.config.ts
@@ -1,7 +1,7 @@
 import {CapacitorConfig} from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-    appId: 'io.ionic.starter',
+    appId: 'com.davidslatinek.ax87',
     appName: 'ax87',
     webDir: 'build',
     bundledWebRuntime: false
